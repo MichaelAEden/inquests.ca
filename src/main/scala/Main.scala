604ea4129efc67d19131a006f1739ee674a6d0a2
Michael Eden <Mikey.Eden@gmail.com>
2019-06-12 17:18:13 -0400

    Convert tabs to spaces

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,26 +13,26 @@ import scala.util.{Failure, Success}
 
 object Main extends App with EnvReader {
 
-	val host = "0.0.0.0"
-	val port = getEnvWithDefault("PORT", "9000").toInt
+  val host = "0.0.0.0"
+  val port = getEnvWithDefault("PORT", "9000").toInt
 
-	implicit val system: ActorSystem = ActorSystem(name = "inquests-ca")
-	implicit val materializer: ActorMaterializer = ActorMaterializer()
-	import system.dispatcher
+  implicit val system: ActorSystem = ActorSystem(name = "inquests-ca")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  import system.dispatcher
 
-	val inquestRepository = new InMemoryInquestRepository(Seq(
-		Inquest(Some(1), "Queen vs CBC", "some inquest"),
-		Inquest(Some(2), "Superman vs Batman", "some inquest"),
-	))
-	val router = new AppRouter(inquestRepository)
-	val server = new Server(router, host, port)
+  val inquestRepository = new InMemoryInquestRepository(Seq(
+    Inquest(Some(1), "Queen vs CBC", "some inquest"),
+    Inquest(Some(2), "Superman vs Batman", "some inquest"),
+  ))
+  val router = new AppRouter(inquestRepository)
+  val server = new Server(router, host, port)
 
-	val binding = server.bind()
+  val binding = server.bind()
 
-	binding.onComplete {
-		case Success(_) => println("Success!")
-		case Failure(error) => println(s"Failed: ${error.getMessage}")
-	}
+  binding.onComplete {
+    case Success(_) => println("Success!")
+    case Failure(error) => println(s"Failed: ${error.getMessage}")
+  }
 
-	Await.result(binding, 10.seconds)
+  Await.result(binding, 10.seconds)
 }
