06fa2321c991f35d0e34aea56e9baf9564b4eacd
Michael Eden <Mikey.Eden@gmail.com>
2019-07-06 12:17:25 -0400

    Refactoring

--- src/main/scala/service/directives/AuthenticationDirectives.scala
+++ src/main/scala/service/directives/AuthenticationDirectives.scala
@@ -10,6 +10,28 @@ import scala.collection.JavaConverters._
 
 trait AuthenticationDirectives extends Directives {
 
+  private def extractUserRecord(idToken: String): Directive1[UserRecord] = {
+    try {
+      val decodedToken = FirebaseAuth.getInstance.verifyIdToken(idToken)
+      val uid = decodedToken.getUid
+      val userRecord = FirebaseAuth.getInstance.getUser(uid)
+      provide(userRecord)
+    } catch {
+      // TODO: log error.
+      case _: FirebaseAuthException =>
+        val apiError = ApiError.authenticationFailure
+        complete(apiError.statusCode, apiError.message)
+      case _: Throwable =>
+        val apiError = ApiError.generic
+        complete(apiError.statusCode, apiError.message)
+    }
+  }
+
+  private def handleUnauthorized(rejections: Seq[Rejection]): Directive1[String] = {
+    val apiError = ApiError.unauthorized
+    complete(apiError.statusCode, apiError.message)
+  }
+
   def authenticateUser: Directive1[UserRecord] = {
 
     def extractBearerToken: HttpHeader => Option[String] = {
@@ -17,28 +39,6 @@ trait AuthenticationDirectives extends Directives {
         Some(token.stripPrefix(s"Bearer "))
       case _ =>
         None
-    }
-
-    def extractUserRecord(idToken: String): Directive1[UserRecord] = {
-      try {
-        val decodedToken = FirebaseAuth.getInstance.verifyIdToken(idToken)
-        val uid = decodedToken.getUid
-        val userRecord = FirebaseAuth.getInstance.getUser(uid)
-        provide(userRecord)
-      } catch {
-        // TODO: log error.
-        case _: FirebaseAuthException =>
-          val apiError = ApiError.authenticationFailure
-          complete(apiError.statusCode, apiError.message)
-        case _: Throwable =>
-          val apiError = ApiError.generic
-          complete(apiError.statusCode, apiError.message)
-      }
-    }
-
-    def handleUnauthorized(rejections: Seq[Rejection]): Directive1[String] = {
-      val apiError = ApiError.unauthorized
-      complete(apiError.statusCode, apiError.message)
     }
 
     headerValue(extractBearerToken)
