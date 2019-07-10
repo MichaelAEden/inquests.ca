42316ea841e7d486ea67ef867d20ec9da2daf6f0
Michael Eden <Mikey.Eden@gmail.com>
2019-07-10 12:33:17 -0400

    Add TODO

--- src/main/scala/service/router/InquestRouter.scala
+++ src/main/scala/service/router/InquestRouter.scala
@@ -16,6 +16,7 @@ class InquestRouter(inquestRepository: InquestRepository, fbClient: FirebaseClie
 
   private implicit val firebaseClient: FirebaseClient = fbClient
 
+  // TODO: sanitize input.
   override def route: Route = pathPrefix("api") {
     pathPrefix("inquests") {
       pathEndOrSingleSlash {
