972860fc2bb4460d5a1bb74bf25c30de08d8f592
Michael Eden <Mikey.Eden@gmail.com>
2019-07-20 22:19:41 -0400

    Refactor InquestTable to new package and remove unused functions

--- src/main/scala/db/slick/InquestTable.scala
+++ src/main/scala/db/slick/InquestTable.scala
@@ -1,11 +1,8 @@
-package db.spec
-
-import slick.lifted.ProvenShape
-import slick.dbio.DBIOAction
+package db.slick
 
 import db.models.Inquest
-
-import scala.concurrent.Future
+import db.spec.Db
+import slick.lifted.ProvenShape
 
 // An Inquest table with 3 columns: id, title, description
 trait InquestTable { this: Db =>
@@ -23,15 +20,5 @@ trait InquestTable { this: Db =>
   }
 
   val inquests = TableQuery[Inquests]
-
-  // TODO: create Table trait with these methods, or move them somewhere more appropriate.
-  // Note these functions would only be used for integration tests.
-  def init(initialInquests: Seq[Inquest] = Seq.empty): Future[Unit] = {
-    db.run(DBIOAction.seq(
-      inquests.schema.create,
-      inquests ++= initialInquests
-    ))
-  }
-  def drop(): Future[Unit] = db.run(DBIOAction.seq(inquests.schema.drop))
 
 }
