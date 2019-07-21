388081bb9eb96f0e5b8a9fd51c6b80169c3c6db4
Michael Eden <Mikey.Eden@gmail.com>
2019-07-21 14:01:13 -0400

    Add method to convert CreateUser request to User model

--- src/main/scala/service/models/CreateUser.scala
+++ src/main/scala/service/models/CreateUser.scala
@@ -1,7 +1,21 @@
 package service.models
 
+import clients.firebase.FirebaseUser
+import db.models.User
+
 case class CreateUser(
-  email: String,
   name: String,
   jurisdiction: String
-)
+) {
+
+  def toUser(firebaseUser: FirebaseUser): User =
+    User(
+      None,
+      firebaseUser.uid,
+      firebaseUser.email,
+      name,
+      jurisdiction,
+      User.UserRole
+    )
+
+}
