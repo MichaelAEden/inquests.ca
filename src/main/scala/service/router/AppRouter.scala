9acceac20534a2a201500109a559a1dfdca989df
Michael Eden <Mikey.Eden@gmail.com>
2019-07-21 14:12:27 -0400

    Include user router in app router

--- src/main/scala/service/router/AppRouter.scala
+++ src/main/scala/service/router/AppRouter.scala
@@ -3,17 +3,22 @@ package service.router
 import akka.http.scaladsl.server.{Directives, Route}
 
 import clients.firebase.FirebaseClient
-import db.spec.InquestRepository
+import db.spec.{InquestRepository, UserRepository}
 
-class AppRouter(inquestRepository: InquestRepository, fbClient: FirebaseClient)
+class AppRouter(
+  inquestRepository: InquestRepository,
+  userRepository: UserRepository,
+  fbClient: FirebaseClient
+)
   extends Router with Directives {
 
   private val staticResourceRouter = StaticResourceRouter()
   private val inquestRouter = new InquestRouter(inquestRepository, fbClient)
+  private val userRouter = new UserRouter(userRepository, fbClient)
 
   // TODO: implement custom exception, rejection handlers.
   override def route: Route = Route.seal {
-    staticResourceRouter.route ~ inquestRouter.route
+    staticResourceRouter.route ~ inquestRouter.route ~ userRouter.route
   }
 
 }
