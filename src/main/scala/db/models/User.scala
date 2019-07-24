58403f921f0b25e9f3e15cd1245a16b4ef8af7a4
Michael Eden <Mikey.Eden@gmail.com>
2019-07-23 20:02:50 -0400

    Add todo

--- src/main/scala/db/models/User.scala
+++ src/main/scala/db/models/User.scala
@@ -2,6 +2,8 @@ package db.models
 
 import java.sql.Timestamp
 
+// TODO: jurisdictionId field should be nullable for users who choose not to
+// TODO: (cont) provide it.
 case class User(
   id: Option[Int],
   firebaseUid: String,
