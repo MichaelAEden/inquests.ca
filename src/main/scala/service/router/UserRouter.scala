215664687bc0c2ea0fd343c3f272dd96e1b68782
Michael Eden <Mikey.Eden@gmail.com>
2019-07-24 12:19:01 -0400

    Add GET, PUT /api/users routes

--- src/main/scala/service/router/UserRouter.scala
+++ src/main/scala/service/router/UserRouter.scala
@@ -3,6 +3,7 @@ package service.router
 import akka.http.scaladsl.server.Route
 
 import clients.firebase.FirebaseClient
+import db.models.Action
 import db.spec.UserRepository
 import service.directives._
 import service.models._
@@ -13,6 +14,7 @@ class UserRouter(userRepository: UserRepository, firebaseClient: FirebaseClient)
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
   import io.circe.generic.auto._
 
+  private implicit val userRepo: UserRepository = userRepository
   private implicit val fbClient: FirebaseClient = firebaseClient
 
   override def route: Route = pathPrefix("api") {
@@ -23,6 +25,38 @@ class UserRouter(userRepository: UserRepository, firebaseClient: FirebaseClient)
             entity(as[CreateUser]) { createUser =>
               validateWith(CreateUserValidator)(createUser) {
                 handleWithGeneric(userRepository.create(createUser, firebaseUser)) { user =>
+                  val userResponse = UserResponse.fromUser(user)
+                  complete(userResponse)
+                }
+              }
+            }
+          }
+        } ~ get {
+          authorizeAction(Action.ManageUsers) apply { _ =>
+            parameters("email") { email =>
+              handle(userRepository.byEmail(email)) {
+                case UserRepository.UserNotFound() =>
+                  ApiError.userNotFound
+                case _ =>
+                  ApiError.generic
+              } { user =>
+                val userResponse = UserResponse.fromUser(user)
+                complete(userResponse)
+              }
+            }
+          }
+        }
+      } ~ path(IntNumber) { id: Int =>
+        put {
+          authorizeAction(Action.ManageUsers) apply { _ =>
+            entity(as[UpdateUser]) { updateUser =>
+              validateWith(UpdateUserValidator)(updateUser) {
+                handle(userRepository.update(id, updateUser)) {
+                  case UserRepository.UserNotFound() =>
+                    ApiError.userNotFound
+                  case _ =>
+                    ApiError.generic
+                } { user =>
                   val userResponse = UserResponse.fromUser(user)
                   complete(userResponse)
                 }
