ecd0c0890303c05d4a0ee69e3fc9366fdc660750
Michael Eden <Mikey.Eden@gmail.com>
2019-08-01 19:10:00 -0400

    Rename inquest table id col

--- src/main/scala/db/slick/InquestTable.scala
+++ src/main/scala/db/slick/InquestTable.scala
@@ -11,7 +11,7 @@ trait InquestTable { this: Db =>
 
   class Inquests(tag: Tag) extends Table[Inquest](tag, "inquest") {
 
-    def id: Rep[Int] = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def id: Rep[Int] = column[Int]("inquest_id", O.PrimaryKey, O.AutoInc)
     def title: Rep[String] = column[String]("title")
     def description: Rep[String] = column[String]("description")
 
